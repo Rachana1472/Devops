dff83c7b2fe4c16d5260b697dee2b97823f4ea88
Rachana1472 <sbrachana571@gmail.com>
2026-01-22 09:36:19 +0530

    Create SimpleProgram.java

---
+++ SimpleProgram.java
@@ -0,0 +1,10 @@
+class SimpleProgram {
+    public static void main(String[] args) {
+        String name = "Java";
+        System.out.println("Hello, " + name);
+
+        int a = 5;
+        int b = 3;
+        System.out.println("Sum of a and b is: " + (a + b));
+    }
+}
